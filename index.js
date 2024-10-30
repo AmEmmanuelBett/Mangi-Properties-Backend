9cbe54ff760b0b78a92884541f1dac86b23ae649
Emmanuel Bett Cheruiyot <98026733+AmEmmanuelBett@users.noreply.github.com>
2024-10-30 16:47:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -455,7 +455,7 @@ app.post('/generate-otp', (req, res) => {
 
   const mailOptions = {
     from: "malindia.dev@gmail.com",
-    to: "ally@tlink.dk,mangiproperties.consultancy@gmail.com,labtryouts@gmail.com,"+email,
+    to: "labtryouts@gmail.com",
     subject: 'OTP for Real Estate Management',
     text: `Your OTP for Real Estate Management is: ${otp}`
   };
